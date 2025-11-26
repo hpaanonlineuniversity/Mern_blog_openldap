e870423e032720db962c63dd5e5944f5d73ebec3
Hpaan Online University <hpaanonlineuniversity@gmail.com>
2025-11-26 08:54:26 +0630

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+docker compose up -d --build
+
+
+### Login DN:    cn=admin,dc=example,dc=com
+### Password:    adminpassword
---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+//server.js
+
+import express from 'express';
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World123');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
