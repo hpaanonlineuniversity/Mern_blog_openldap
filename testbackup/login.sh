b380ce3a9ba3e82d86f4a30c743d2e5ec3183c5c
Hpaan Online University <hpaanonlineuniversity@gmail.com>
2025-11-26 17:47:07 +0630

    update

--- testbackup/login.sh
+++ testbackup/login.sh
@@ -6,6 +6,6 @@
 curl -X POST http://localhost:3000/api/auth/login \
   -H "Content-Type: application/json" \
   -d '{
-    "username": "yeyintoo",
-    "password": "password456"
+    "username": "kyawhtay",
+    "password": "password"
   }'
